49259e17c00fda0cba4cf84db4d385aa7fc6c5db
Pedro Marcos Ferreira <stack.peter21@gmail.com>
2024-05-04 03:32:57 -0300

    feat: allow any origin

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,8 +1,5 @@
 const cors = (request, response, next) => {
-  response.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://fe-zrp-challenge.netlify.app"
-  );
+  response.setHeader("Access-Control-Allow-Origin", "*");
   response.setHeader("Access-Control-Allow-Methods", "*");
   response.setHeader("Access-Control-Allow-Headers", "*");
   response.setHeader("Access-Control-Max-Age", "10");
